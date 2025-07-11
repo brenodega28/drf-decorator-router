a04b18c5da2f359aa212935406040f3adf0ff603
Breno Gomes <brenodega28@gmail.com>
2025-07-11 18:44:23 -0300

    feat: admin router

--- tests/urls.py
+++ tests/urls.py
@@ -18,9 +18,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from .mockapp.routers import router
+from .mockapp.routers import admin_router, router
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    router.path
+    router.path,
+    admin_router.path
 ]
